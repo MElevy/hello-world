f94b0ffb857cc656935612aa1f0d6ecd61118c2e
MElevy <deathmoonlevy@gmail.com>
2022-04-27 17:50:05 -0700

    cleaned up code

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,9 @@
 from kivy.app import App
 from kivy.uix.button import Button
+
 class myApp(App):
     def build(self):
-    	return Button(text='hello world')
-if __name__=='__main__':
+    	return Button(text = 'hello world')
+
+if __name__ ==  '__main__':
     myApp().run()
