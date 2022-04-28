3f3377269122265ae9436b3bfe9c3cb6c0848c61
MElevy <deathmoonlevy@gmail.com>
2022-04-27 17:54:41 -0700

    Add comments

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,11 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.button import Button # import modules
 
 class MyApp(App):
+    '''A basic app'''
     def build(self):
+        '''Returns a button'''
     	return Button(text = 'hello world')
 
 if __name__ ==  '__main__':
-    MyApp().run()
+    MyApp().run() # runs app
